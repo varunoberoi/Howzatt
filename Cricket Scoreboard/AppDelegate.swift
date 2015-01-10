3857b4d0de6c2b06b573c4ae9963e1cbe2190f12
Varun Oberoi <varun@rubird.com>
2015-01-10 23:38:55 +0530

    Initial Commit

---
+++ Cricket Scoreboard/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cricket Scoreboard
+//
+//  Created by Varun Oberoi on 10/01/15.
+//  Copyright (c) 2015 Varun Oberoi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
