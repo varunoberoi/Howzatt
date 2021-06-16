cd04dafd0d60fbbe73f65b7466f7f82a6b0687f0
Inder Dhir <inderdhir@hotmail.com>
2021-06-16 19:37:04 -0400

    Update code

---
+++ Cricket Scoreboard/Flags.swift
@@ -0,0 +1,22 @@
+//
+//  Flags.swift
+//  Cricket Scoreboard
+//
+//  Created by Inder Dhir on 6/16/21.
+//  Copyright © 2021 Varun Oberoi. All rights reserved.
+//
+
+let flags = [
+    "aus": "🇦🇺",
+    "ind": "🇮🇳",
+    "sa": "🇿🇦",
+    "nz": "🇳🇿",
+    "sl": "🇱🇰",
+    "eng": "🏴󠁧󠁢󠁥󠁮󠁧󠁿",
+    "ban": "🇧🇩",
+    "pak": "🇵🇰",
+    "wi": "",
+    "ire": "🇮🇪",
+    "zim": "🇿🇼",
+    "afg": "🇦🇫"
+]
---
+++ Cricket Scoreboard/ScoreElement.swift
@@ -0,0 +1,11 @@
+//
+//  ScoreElement.swift
+//  Cricket Scoreboard
+//
+//  Created by Inder Dhir on 6/16/21.
+//  Copyright © 2021 Varun Oberoi. All rights reserved.
+//
+
+enum ScoreElement: String {
+    case title, guid
+}
